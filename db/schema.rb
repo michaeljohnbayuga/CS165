b2d8e0de0c0cbf15ea2b7efb84dce3c533bc0ae2
Liaa Navarro <liaanavarro@Liaas-MacBook-Pro-2.local>
2018-02-22 20:32:36 +0800

    season and episode models; tracker mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222052940) do
+ActiveRecord::Schema.define(version: 20180222122536) do
+
+  create_table "episodes", force: :cascade do |t|
+    t.string "show_id"
+    t.integer "season_no"
+    t.integer "episode_no"
+    t.integer "showep_no"
+    t.string "title"
+    t.date "air_date"
+    t.integer "season_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["season_id"], name: "index_episodes_on_season_id"
+  end
+
+  create_table "seasons", force: :cascade do |t|
+    t.integer "show_id"
+    t.integer "season_no"
+    t.integer "no_of_episodes"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["show_id"], name: "index_seasons_on_show_id"
+  end
 
   create_table "shows", force: :cascade do |t|
     t.string "show_id"
@@ -20,6 +44,17 @@ ActiveRecord::Schema.define(version: 20180222052940) do
     t.string "end_year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "trackers", force: :cascade do |t|
+    t.string "username"
+    t.string "show_id"
+    t.integer "season_no"
+    t.integer "episode_no"
+    t.integer "episode_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["episode_id"], name: "index_trackers_on_episode_id"
   end
 
 end
