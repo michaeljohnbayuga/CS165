df78bec214f61972daaf28c35ad3bd6c07f272c9
Liaa Navarro <liaanavarro@Liaas-MacBook-Pro-2.local>
2018-03-02 00:19:33 +0800

    home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,22 @@
 class HomeController < ApplicationController
   def show
-    @episodes = Episode.where(air_date = Date.today.strftime("%Y-%m-%d"))
+    @today = Episode.where(:air_date => Date.today.strftime("%Y-%m-%d"))
+
+    @upcoming = []
+    @dates = []
+    @week = Date.today.at_beginning_of_week..Date.today.at_end_of_week
+
+    @week.map.each do |day|
+      if day > Date.today
+        @dates.append(day.strftime("%Y-%m-%d"))
+        @eps = Episode.where(:air_date => day.strftime("%Y-%m-%d"))
+        if @eps.exists?
+          @eps.each do |ep|
+            @upcoming.append(ep)
+          end
+        end
+      end
+    end
+
   end
 end
