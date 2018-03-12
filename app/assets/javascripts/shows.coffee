cb41caf301638969fbe97d825e608347811600f2
Liaa Navarro <liaanavarro@Liaas-MacBook-Pro-2.local>
2018-03-12 15:12:27 +0800

    redesign view for specific show and tracker

--- app/assets/javascripts/shows.coffee
+++ app/assets/javascripts/shows.coffee
@@ -2,4 +2,6 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 $ ->
-  $('.ui.accordion').accordion();
+  $('.special.cards .card').dimmer({
+    on: 'hover'
+  });
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -7,7 +7,7 @@ class ShowsController < ApplicationController
 
   def show
     @show = Show.find(params[:id])
-    @seasons = @show.seasons
+    @seasons = @show.seasons.order(season_no: :asc)
   end
 
   def new
@@ -37,20 +37,6 @@ class ShowsController < ApplicationController
   def destroy
     @show = Show.find(params[:id])
     redirect_to '/shows'
-  end
-
-  def watch
-    @tracker = Tracker.new
-    @tracker.user_id = current_user.id
-    @tracker.episode_id = params[:id]
-    @tracker.save
-    redirect_back(fallback_location: root_path)
-  end
-
-  def unwatch
-    @tracker = Tracker.find(params[:id])
-    @tracker.destroy
-    redirect_back(fallback_location: root_path)
   end
 
   private
